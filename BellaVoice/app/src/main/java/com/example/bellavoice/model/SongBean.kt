a3b93f1565ad1dff85e488959ca477e7baec8f07
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-11 20:50:01 +0800

    歌曲实体

---
+++ BellaVoice/app/src/main/java/com/example/bellavoice/model/SongBean.kt
@@ -0,0 +1,9 @@
+package com.example.bellavoice.model
+
+data class SongBean @JvmOverloads constructor(
+    val id: Long = 0L,
+    val uri: String = "",
+    val song: String = "", //歌名
+    val singer: String = "", //歌手名称
+    val duration: String = "", //时常
+)
