3e82bd6bc777da6a9d3f35014601c53c4265027b
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-11 20:48:08 +0800

    手动请求访问权限

--- BellaVoice/app/src/main/java/com/example/bellavoice/screen/MainPage.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/screen/MainPage.kt
@@ -1,9 +1,12 @@
 package com.example.bellavoice.screen
 
-import android.content.res.Resources.Theme
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,22 +16,27 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.AlertDialogDefaults.titleContentColor
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonElevation
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.bellavoice.Utils.LocalNavController
 import com.example.bellavoice.component.VoiceCard
+import com.example.bellavoice.viewmodel.SongsViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
@@ -37,6 +45,20 @@ fun MainPage(
     modifier: Modifier = Modifier
 ) {
     val navController = LocalNavController.current
+    val context = LocalContext.current
+    val songVM = SongsViewModel()
+    val coroutineScope = rememberCoroutineScope()
+    val refreshLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGrant ->
+        if (isGrant) {
+            coroutineScope.launch { songVM.loadLocalSongs() }
+        } else {
+            Toast.makeText(context, "请授予权限", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -57,7 +79,34 @@ fun MainPage(
                     Text(text = "首页")
                 },
                 windowInsets = TopAppBarDefaults.windowInsets,
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.LightGray)
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.LightGray
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {
+                            when {
+                                ContextCompat.checkSelfPermission(
+                                    context,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE
+                                ) == PackageManager.PERMISSION_GRANTED -> {
+                                    coroutineScope.launch { songVM.loadLocalSongs() }
+                                }
+
+                                else -> {
+                                    refreshLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                                }
+                            }
+                        }
+                    ) {
+                        Image(
+                            imageVector = Icons.Default.Refresh,
+                            contentDescription = null,
+                            colorFilter = ColorFilter.tint(Color.Black)
+                        )
+                    }
+                }
+
             )
         }
     ) { it ->
