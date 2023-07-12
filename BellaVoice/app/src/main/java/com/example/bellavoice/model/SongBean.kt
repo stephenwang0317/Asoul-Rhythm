9cf27c1be812db44caac13066011589deeaacfc9
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-13 00:23:57 +0800

    修改音乐实体类

--- BellaVoice/app/src/main/java/com/example/bellavoice/model/SongBean.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/model/SongBean.kt
@@ -6,4 +6,5 @@ data class SongBean @JvmOverloads constructor(
     val song: String = "", //歌名
     val singer: String = "", //歌手名称
     val duration: String = "", //时常
+    val path: String = ""
 )
