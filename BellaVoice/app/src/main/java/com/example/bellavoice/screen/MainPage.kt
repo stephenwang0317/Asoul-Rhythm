386cb01a3a2cb9f9ba6b4c850259582f4640fa70
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-13 19:10:41 +0800

    加入BottomBar

--- BellaVoice/app/src/main/java/com/example/bellavoice/screen/MainPage.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/screen/MainPage.kt
@@ -6,7 +6,6 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,37 +13,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Pause
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import com.example.bellavoice.R
 import com.example.bellavoice.Utils.LocalNavController
-import com.example.bellavoice.component.BottomBar
+import com.example.bellavoice.component.BoxWithBottomBar
 import com.example.bellavoice.component.VoiceCard
 import com.example.bellavoice.model.SongBean
 import com.example.bellavoice.viewmodel.SongsViewModel
@@ -73,23 +66,20 @@ fun MainPage(
     val targetSong by songVM.targetSong.observeAsState(ArrayList<SongBean>())
 
     Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { navController.navigate("AddPage") },
-                shape = CircleShape,
-                containerColor = Color.LightGray,
-            ) {
-                Image(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null,
-                    modifier = Modifier.size(30.dp)
-                )
-            }
-        },
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate("AddPage") }) {
+                        Image(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                },
                 title = {
-                    Text(text = "首页")
+                    Row() {
+                        Image(painter = painterResource(id = R.drawable._ava), contentDescription = null)
+                        Image(painter = painterResource(id = R.drawable._bella), contentDescription = null)
+                        Image(painter = painterResource(id = R.drawable._diana), contentDescription = null)
+                        Image(painter = painterResource(id = R.drawable._elieen), contentDescription = null)
+                    }
                 },
                 windowInsets = TopAppBarDefaults.windowInsets,
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -110,7 +100,7 @@ fun MainPage(
                                     refreshLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
                                 }
                             }
-                        }
+                        },
                     ) {
                         Image(
                             imageVector = Icons.Default.Refresh,
@@ -144,13 +134,7 @@ fun MainPage(
                     )
                     .weight(1f)
             )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-            ) {
-                BottomBar(songVM)
-            }
         }
+        BoxWithBottomBar()
     }
 }
