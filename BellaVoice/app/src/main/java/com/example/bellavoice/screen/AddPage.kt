688743151718838611cc168d9b94023a62def252
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-13 19:08:47 +0800

    更新UI，把TextField换成Text,支持自定义文件名

--- BellaVoice/app/src/main/java/com/example/bellavoice/screen/AddPage.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/screen/AddPage.kt
@@ -3,6 +3,8 @@ package com.example.bellavoice.screen
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,21 +15,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,6 +39,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,8 +74,8 @@ fun AddPage(
         )
         OutlinedTextField(
             modifier = Modifier.padding(top = 10.dp),
-            value = downloadVM.text,
-            onValueChange = { downloadVM.text = it },
+            value = downloadVM.bv,
+            onValueChange = { downloadVM.bv = it },
             singleLine = true,
             label = { Text(text = "BV号") },
             textStyle = TextStyle(
@@ -99,11 +104,13 @@ fun AddPage(
             CircularProgressIndicator(modifier.size(50.dp))
         } else {
 //            SingleLine(title = "bv号", msg = downloadVM.text)
-            SingleLine(title = "时间", msg = timeStamp2DateStr(downloadVM.result.timestamp))
-            SingleLine(title = "信息", msg = downloadVM.result.message)
-            SingleLine(title = "code", msg = downloadVM.result.status.toString())
-            SingleLine(title = "url", msg = downloadVM.result.data?.url ?: "")
-            SingleLine(title = "标题", msg = downloadVM.result.data?.video_title ?: "")
+            MyText(title = "时间", msg = timeStamp2DateStr(downloadVM.result.timestamp))
+            MyText(title = "信息", msg = downloadVM.result.message)
+            MyText(title = "code", msg = downloadVM.result.status.toString())
+            SingleLine(title = "url", msg = downloadVM.result.data?.url ?: "", readable = true)
+            SingleLine(title = "标题", msg = downloadVM.fileName){
+                downloadVM.fileName = it
+            }
         }
 
         Spacer(modifier = Modifier.height(20.dp))
@@ -141,11 +148,15 @@ fun timeStamp2DateStr(timeStamp: Double): String {
 
 @Composable
 fun SingleLine(
-    title: String = "1231231",
-    msg: String = "sdasdasdasdas"
+    title: String = "title",
+    msg: String = "msg",
+    readable: Boolean = false,
+    onValueChange: (String) -> Unit = {}
 ) {
     Row(
-        modifier = Modifier.padding(bottom = 15.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(bottom = 15.dp)
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -156,11 +167,53 @@ fun SingleLine(
         )
         TextField(
             value = msg,
-            readOnly = true,
-            onValueChange = {},
+            readOnly = readable,
+            onValueChange = onValueChange,
             singleLine = true,
             shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f),
+            colors = TextFieldDefaults.colors(unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer)
         )
+    }
+}
+
+@Composable
+fun MyText(
+    title: String = "1231231",
+    msg: String = "sdasdasdasdas"
+) {
+    Row(
+        modifier = Modifier
+            .padding(bottom = 15.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title, fontSize = 20.sp,
+            modifier = Modifier
+                .padding(end = 10.dp)
+                .width(60.dp)
+        )
+        Column(
+            modifier = Modifier
+                .wrapContentWidth()
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(10)
+                ),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = msg,
+                modifier = Modifier
+                    .height(40.dp)
+                    .padding(vertical = 5.dp)
+                    .padding(start = 5.dp),
+                maxLines = 1,
+                overflow = TextOverflow.Visible,
+                fontSize = 20.sp,
+            )
+            Divider(thickness = 1.dp, color = Color.DarkGray)
+        }
     }
 }
