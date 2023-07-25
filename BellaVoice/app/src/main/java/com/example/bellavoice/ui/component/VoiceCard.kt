d91d23dd8bd3419105e6494bc4b1b03017a4402a
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-26 00:54:21 +0800

    修改DropdownMenu的子项，Edit子项跳转ChangeImage页面

--- BellaVoice/app/src/main/java/com/example/bellavoice/ui/component/VoiceCard.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/ui/component/VoiceCard.kt
@@ -1,7 +1,6 @@
 package com.example.bellavoice.ui.component
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -34,7 +33,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -42,12 +40,14 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.bellavoice.R
 import com.example.bellavoice.model.SongBean
+import com.example.bellavoice.myutils.LocalNavController
 import com.example.bellavoice.viewmodel.SongsViewModel
 
 @Composable
 fun MyDropdownMenu(
     expand: Boolean,
-    onDismissRequest: () -> Unit
+    onDismissRequest: () -> Unit,
+    onClick: () -> Unit
 ) {
     DropdownMenu(
         expanded = expand,
@@ -56,7 +56,7 @@ fun MyDropdownMenu(
     ) {
         DropdownMenuItem(
             text = { Text("Edit") },
-            onClick = { /* Handle edit! */ },
+            onClick = onClick,
             leadingIcon = {
                 Icon(
                     Icons.Outlined.Edit,
@@ -66,20 +66,10 @@ fun MyDropdownMenu(
         )
         DropdownMenuItem(
             text = { Text("Delete") },
-            onClick = { /* Handle edit! */ },
+            onClick = { },
             leadingIcon = {
                 Icon(
                     Icons.Outlined.Delete,
-                    contentDescription = null
-                )
-            }
-        )
-        DropdownMenuItem(
-            text = { Text("Favorite") },
-            onClick = { /* Handle edit! */ },
-            leadingIcon = {
-                Icon(
-                    Icons.Outlined.Favorite,
                     contentDescription = null
                 )
             }
@@ -95,6 +85,7 @@ fun VoiceCard2(
     var expand by remember {
         mutableStateOf(false)
     }
+    val navController = LocalNavController.current
 
     Column(
         modifier = Modifier
@@ -162,7 +153,11 @@ fun VoiceCard2(
                     tint = MaterialTheme.colorScheme.outline,
                     modifier = Modifier.clickable { expand = true }
                 )
-                MyDropdownMenu(expand = expand) { expand = false }
+                MyDropdownMenu(
+                    expand = expand,
+                    onDismissRequest = { expand = false },
+                    onClick = { navController.navigate("ChangeImage/" + bean.id) }
+                )
             }
         }
         Divider(modifier = Modifier.fillMaxWidth(), thickness = 1.dp)
