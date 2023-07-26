2d382b1fad8803a40ed6d2bc42827797b636064f
stephenwang0317 <stephenwang0317@gmail.com>
2023-07-27 01:31:13 +0800

    修改UI

--- BellaVoice/app/src/main/java/com/example/bellavoice/ui/screen/ChangeImage.kt
+++ BellaVoice/app/src/main/java/com/example/bellavoice/ui/screen/ChangeImage.kt
@@ -1,14 +1,18 @@
 package com.example.bellavoice.ui.screen
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +24,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,14 +37,15 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.bellavoice.R
+import com.example.bellavoice.model.SongBean
 import com.example.bellavoice.myutils.LocalDownloadViewModel
 import com.example.bellavoice.myutils.LocalNavController
 import com.example.bellavoice.myutils.LocalSongsViewModel
 import kotlinx.coroutines.launch
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChangeImage(
@@ -49,13 +55,31 @@ fun ChangeImage(
     var bv by remember {
         mutableStateOf("")
     }
-    var img_url by remember {
-        mutableStateOf("")
-    }
 
     val downloadVM = LocalDownloadViewModel.current
     val songVM = LocalSongsViewModel.current
     val coroutineScope = rememberCoroutineScope()
+    var bean = SongBean()
+
+    var songName by remember {
+        mutableStateOf("")
+    }
+    var imgUrl by remember {
+        mutableStateOf("")
+    }
+    var songNameChanged by remember {
+        mutableStateOf(false)
+    }
+    var imgUrlChanged by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        bean = songVM.getSingleSong(id)
+        songName = bean.song
+        imgUrl = bean.uri
+    }
+
 
     Scaffold(
         topBar = {
@@ -69,7 +93,23 @@ fun ChangeImage(
                         )
                     }
                 },
-                title = { Text(text = "修改封面") },
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.loading),
+                            contentDescription = null,
+                            modifier = Modifier.size(50.dp)
+                        )
+                        Text(text = "修改信息", fontSize = 25.sp)
+                        Image(
+                            painter = painterResource(id = R.drawable.loading),
+                            contentDescription = null,
+                            modifier = Modifier.size(50.dp)
+                        )
+                    }
+                },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
@@ -80,32 +120,66 @@ fun ChangeImage(
         }
     ) {
         Column(
-            modifier = Modifier.padding(20.dp),
+            modifier = Modifier.padding(it),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             MainPageTitle(title = "修改封面")
-            TextField(
-                value = bv,
-                onValueChange = { bv = it },
-                leadingIcon = {
-                    Image(imageVector = Icons.Default.Image, contentDescription = null)
-                },
-                label = {
-                    Text(text = "bv号")
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 5.dp)
+            ) {
+                Spacer(modifier = Modifier.fillMaxWidth(0.1f))
+                TextField(
+                    value = bv,
+                    onValueChange = { bv = it },
+                    leadingIcon = {
+                        Image(imageVector = Icons.Default.Image, contentDescription = null)
+                    },
+                    label = {
+                        Text(text = "bv号")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = { bv = "" }) {
+                            Image(imageVector = Icons.Default.Close, contentDescription = null)
+                        }
+                    }
+                )
+                TextButton(
+                    onClick = {
+                        coroutineScope.launch {
+                            val result = downloadVM.getUrl(bv)
+                            imgUrl = result.data?.img ?: ""
+                            imgUrlChanged = true
+                        }
+                    }
+                ) {
+                    Text("解析")
                 }
-            )
+            }
             AsyncImage(
-                model = img_url,
+                model = imgUrl,
                 contentDescription = null,
                 placeholder = painterResource(id = R.drawable.album_default),
                 error = painterResource(id = R.drawable.album_default),
                 modifier = Modifier
                     .aspectRatio(1.6f)
                     .clip(
-                        RoundedCornerShape(20)
+                        RoundedCornerShape(10)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(10)
                     ),
                 contentScale = ContentScale.Crop
             )
+            EditTextField(
+                str = songName,
+                label = "名字",
+                onChange = { songName = it; songNameChanged = true },
+                onClose = { songName = ""; songNameChanged = true },
+            )
+
             Row() {
                 TextButton(
                     onClick = { navController.popBackStack() }
@@ -115,18 +189,8 @@ fun ChangeImage(
                 TextButton(
                     onClick = {
                         coroutineScope.launch {
-                            val result = downloadVM.getUrl(bv)
-                            img_url = result.data?.img ?: ""
-                        }
-                    }
-                ) {
-                    Text("解析")
-                }
-                TextButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            val bean = songVM.getSingleSong(id)
-                            bean.uri = img_url
+                            if (imgUrlChanged) bean.uri = imgUrl
+                            if (songNameChanged) bean.song = songName
                             songVM.upsert(bean)
                         }
                     }
@@ -136,4 +200,31 @@ fun ChangeImage(
             }
         }
     }
+}
+
+@Composable
+fun EditTextField(
+    modifier: Modifier = Modifier,
+    str: String,
+    label: String,
+    onChange: (String) -> Unit,
+    onClose: () -> Unit
+) {
+    TextField(
+        value = str,
+        onValueChange = { onChange(it) },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 10.dp, vertical = 5.dp
+            ),
+        trailingIcon = {
+            IconButton(onClick = onClose) {
+                Image(imageVector = Icons.Default.Close, contentDescription = null)
+            }
+        },
+        label = {
+            Text(text = label)
+        }
+    )
 }
